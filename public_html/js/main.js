e56d34ce27f683374e5e04d014b93e39ec8daa22
Stefan Ristic <ristic.stef@gmail.com>
2020-11-03 20:10:25 +0100

    Madmoisele final version

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -33,15 +33,46 @@ $(document).ready(function(){
         });
     }
 
-    // if($('.blog-slider').length > 0) {
-    //     $('.owl-carousel').owlCarousel({
+    // FORM VALIDATOR
+    if($('.contact-form').length > 0){
 
-    //         loop: true,
-    //         autoplay: false,
-    //         nav: true,
-    //         items: 1,
-    //         margin: 0
-    //     });
-    // }
+        $('.contact-form').validate({
+
+             highlight: function(element){
+                $(element).addClass('is-invalid').removeClass('is-valid')
+            },
+            unhighlight: function(element){
+                $(element).addClass('is-valid').removeClass('is-invalid')
+            },
+            rules: {
+                name: {
+                    required: true
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                message: {
+                    required: true    
+                }
+            },
+            messages: {
+                name: {
+                    required: 'The Name* field is required'
+                },
+                email: {
+                    required: 'The Email* field is required',
+                    email: 'Please provide a valid email address'
+                },
+                message: {
+                    required: 'The Message* field is required'
+                }
+            },
+            errorElement: 'p',
+            errorPlacement: function(error, element) {
+                error.appendTo( element.closest('.form-group').find('.error-msg') );
+              }
+        })
+    }
 
 })
